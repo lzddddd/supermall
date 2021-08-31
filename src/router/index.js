ae38a66256a43eff9d5f38edc946726efdae6f5d
lzddddd <1916959422@qq.com>
2021-09-01 00:40:56 +0800

    second commit: develop tabbar(page bottom)

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+// 懒加载
+const Home = () => import('views/home/Home')
+const Itemize = () => import('views/itemize/Itemize')
+const Cart = () => import('views/cart/Cart')
+const Profile = () => import('views/profile/Profile')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/itemize',
+    name: 'Itemize',
+    component: Itemize
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/itemize',
+    name: 'Itemize',
+    component: Itemize
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
   }
 ]
 
